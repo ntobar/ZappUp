a69b6cd84db605ab74b6602ccfe0b8586441f85c
Nicolas Tobar <tobar.n@husky.neu.edu>
2019-03-14 17:17:24 -0400

    This is a new commit

---
+++ app/src/main/java/com/tobar/woke/woke/SpashActivity.java
@@ -0,0 +1,56 @@
+package com.tobar.woke.woke;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SpashActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME_OUT = 3000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Toast.makeText(getApplicationContext(), "Loading Woke...", Toast.LENGTH_LONG).show();
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent homeIntent = new Intent(SpashActivity.this, CurrentActivity.class);
+                startActivity(homeIntent);
+                finish();
+
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+    }
+
+}
