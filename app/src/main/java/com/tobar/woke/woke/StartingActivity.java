baaf9b4c5c616de0c6a51f15328e7b99724c1071
Nicolas Tobar <tobar.n@husky.neu.edu>
2019-03-20 16:17:04 -0400

    splash to Start done, now start to login

--- app/src/main/java/com/tobar/woke/woke/StartingActivity.java
+++ app/src/main/java/com/tobar/woke/woke/StartingActivity.java
@@ -2,12 +2,24 @@ package com.tobar.woke.woke;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class StartingActivity extends AppCompatActivity {
+public class StartingActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            // ...
+        }
+
     }
 }
